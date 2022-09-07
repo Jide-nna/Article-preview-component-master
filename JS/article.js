4be1d495b798d244358f19dc11fbf1b57ac830b3
Bakinson Babajide <93973753+Jide-nna@users.noreply.github.com>
2022-09-07 20:33:41 +0100

    initail commit

--- JS/article.js
+++
@@ -1,7 +0,0 @@
-var share = document.querySelector(".hidden")
-var btn = document.querySelector('.share')
-var menu = document.querySelector('.active')
-btn.addEventListener("click", function() {
-    btn.classList.toggle("btn")
-    share.classList.toggle("active")
-})
--- README.md
+++
@@ -1,2 +0,0 @@
-# Article preview component master
- 
